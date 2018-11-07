fa1af16e0b2cc6e214cff72d9617cc6878112472
Yevgeniy Ivanov <anigeka@gmail.com>
2018-11-07 14:51:28 -0600

    mostly complete

--- 01-data-structures/04-hashes-part-1/hash_item.rb
+++ 01-data-structures/04-hashes-part-1/hash_item.rb
@@ -3,5 +3,17 @@ class HashItem
   attr_accessor :value
 
   def initialize(key, value)
+    @key = key
+    @value = value
+    HashItem.all << self
   end
+
+  def self.all 
+    @all ||= []
+  end
+
+  def self.each(&proc)
+    @all.each(&proc)
+  end
+  
 end
--- 01-data-structures/04-hashes-part-1/hashclass.rb
+++ 01-data-structures/04-hashes-part-1/hashclass.rb
@@ -1,27 +1,58 @@
 class HashClass
+  require_relative 'hash_item'
 
   def initialize(size)
-    @items = Array.new(size)
+    @size = size
+    @items = Array.new(@size)
   end
 
   def []=(key, value)
+    HashItem.new(key, value)
+    new_index = index(key, @size)
+    if(@items[new_index] == nil)
+      @items[new_index] = value
+    else
+      resize
+    end
   end
 
-
   def [](key)
+    new_index = index(key, @size)
+    @items[new_index]
   end
 
   def resize
+   @size = @size * 2
+   @items = Array.new(@size)
+   resize_indexer
   end
 
   # Returns a unique, deterministically reproducible index into an array
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
+  def resize_indexer
+    HashItem.each do |item|
+      new_index = index(item.key, @size)
+      if(@items[new_index] == nil)
+        @items[new_index] = item.value
+      else
+        resize 
+      end
+    end
+  end
+
   def index(key, size)
+    holder = 0
+    key.to_s.each_byte do |c|
+      holder += c
+    end
+    new_index = holder % size
   end
 
   # Simple method to return the number of items in the hash
   def size
+    @size                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
   end
+  
 
 end
